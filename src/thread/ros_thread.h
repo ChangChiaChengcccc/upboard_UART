fd51932b48a4e384b58a7a8469fd6c93101e49a3
lioujg <lioujg870117@gmail.com>
2021-05-17 15:55:47 +0800

    Revert "change the method of packaging messages and add the controller force message"
    This reverts commit 62b1e017e319c746483bc74f30be2ad13d503459.

--- src/thread/ros_thread.h
+++ src/thread/ros_thread.h
@@ -1,9 +1,7 @@
 #include "geometry_msgs/Point.h"
-#include <queue>
 #ifndef __ROS_THREAD_H__
 #define __ROS_THREAD_H__
-void ukf_force_callback(geometry_msgs::Point force);
-void controller_force_callback(geometry_msgs::Point force);
+void force_callback(geometry_msgs::Point force);
 
 int ros_thread_entry();
 
