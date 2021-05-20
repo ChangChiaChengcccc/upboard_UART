98ec7abf091ef46d3e315bc562596cfebeaf45d4
lioujg <lioujg870117@gmail.com>
2021-05-20 17:08:18 +0800

    modify the callback function with the get parameter

--- src/thread/ros_thread.cpp
+++ src/thread/ros_thread.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 mutex ros_mutex;
 queue<float> send_to_stm32;
+static std::string MAV_;
 
 void ukf_force_callback(geometry_msgs::Point force)
 {
@@ -20,7 +21,12 @@ void ukf_force_callback(geometry_msgs::Point force)
 	// send_to_stm32.push(force.z);
 	// send_pose_to_serial(send_to_stm32);
 	// send_to_stm32 = queue<float>();
-	send_pose_to_serial(force.x, force.y, force.z);
+	if(MAV_ == "follower")
+	{
+		send_pose_to_serial(force.x, force.y, force.z);
+	}
+	//send_pose_to_serial(force.x, force.y, force.z);
+	//ROS_INFO_STREAM("MAV is " << MAV_);
 }
 
 void controller_force_callback(geometry_msgs::Point force)
@@ -30,25 +36,19 @@ void controller_force_callback(geometry_msgs::Point force)
 	// send_to_stm32.push(force.z);
 	// send_pose_to_serial(send_to_stm32);
 	// send_to_stm32 = queue<float>();
-	send_pose_to_serial(force.x, force.y, force.z);
+	if(MAV_ == "leader")
+	{
+		send_pose_to_serial(force.x, force.y, force.z);
+	}
+	//send_pose_to_serial(force.x, force.y, force.z);
 }
 
 int ros_thread_entry(){
 	ros::NodeHandle n;
-	std::string MAV = "";
-	n.getParam("MAV", MAV);
-	if(MAV == "leader")
-	{
-		ros::Subscriber ctrl_sub = n.subscribe("/controller_force",1000,controller_force_callback);
-	}
-	else if(MAV == "follower")
-	{
-		ros::Subscriber ukf_sub = n.subscribe("force_estimate",1000,ukf_force_callback);
-	}
-	else
-	{
-		ROS_INFO("Error! Please input the MAV type ex.leader, follower");
-	}
+	n.param<std::string>("MAV", MAV_, "follower");
+	//n.getParam("MAV", MAV);
+	ros::Subscriber ctrl_sub = n.subscribe("/controller_force",1000,controller_force_callback);
+	ros::Subscriber ukf_sub = n.subscribe("force_estimate",1000,ukf_force_callback);
 	ros::spin();
 	return 0;
 }
